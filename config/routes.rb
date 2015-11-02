9be0bb3de8acac73a38e5cc4eae6c6ca8b84198c
AA <andrewhkg@hotmail.com>
2015-11-02 18:25:47 +0800

    added session controllers ans signin and signout buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
   root 'static_pages#index'
 
   resources :users, only: [:new, :create]
 
-  get "/signup", to: "users#new"
+  get "signup", to: "users#new"
 
+  get "login", to: "sessions#new"
+
+  delete "logout", to: "sessions#destroy"
+
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
