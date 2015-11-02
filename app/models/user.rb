835582712c5b3981da39433530be54c189bd59c2
AA <andrewhkg@hotmail.com>
2015-11-02 17:49:13 +0800

    signup page created, nabar updated, user model updated

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
