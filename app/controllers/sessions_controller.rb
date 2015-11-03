a52d86daf9f9ac1b40e134e231bc5a01f683049e
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-03 17:30:13 +0800

    Styling for errors fixed.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,17 @@ class SessionsController < ApplicationController
   end
 
    # Authentication logic
- def create
-  user = User.find_by_email(params[:email])
-
-  # email_found && params[:password] == hashed_password ?
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to "/"
-    flash[:success] = "Logged in!"
-  else
-    flash[:danger] = "Invalid login credentials."
-    render "new"  # sessions#new
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to "/"
+      flash[:success] = "Logged in!"
+    else
+      flash[:danger] = "Invalid login credentials."
+      render "new"  # sessions#new
+    end
   end
-
- end
 
  def destroy
   session[:user_id] = nil
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
 
-
   def new
     @user = User.new
   end
 
   def create
-  @user = User.new user_params
-  if @user.save
-    flash[:success] = "You have successfully signed up!"
-    redirect_to "/"
-  else
-    render 'new'
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "You have successfully signed up!"
+      redirect_to "/"
+    else
+      render 'new'
+    end
   end
- end
 
 private
  def user_params
