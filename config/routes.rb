2ee78e15eff96459b650367bce7f929a4a1e29ff
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-04 10:34:37 +0800

    user is able to book a tour and see tour details.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-
   root 'static_pages#index'
+
 
   resources :users,    only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :tours,    only: [:index, :show]
+  resources :tours,    only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:index, :show]
 
   get "signup",    to: "users#new"
   get "login",     to: "sessions#new"
